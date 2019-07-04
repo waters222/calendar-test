763395d64bc7e45e1ab63d380e599e3ed6564bbd
wei.shi <weishi258@gmail.com>
2019-07-04 19:45:06 -0400

    add segments overlap checking algorithm

--- calendar.go
+++ calendar.go
@@ -26,24 +26,6 @@ func (c *CalendarEvent) isOverlap(evt CalendarEvent) bool {
 		evt.End >= c.Start && evt.End <= c.End
 }
 
-// using Golang's sort interface
-// we sort the calendar events by using Start date
-type CalendarEvents []CalendarEvent
-
-func (c CalendarEvents) Len() int {
-	return len(c)
-}
-
-func (c CalendarEvents) Swap(i, j int) {
-	temp := c[i]
-	c[i] = c[j]
-	c[j] = temp
-}
-
-func (c CalendarEvents) Less(i, j int) bool {
-	return c[i].Start < c[j].Start
-}
-
 // struct for return pair
 type CalendarPair struct {
 	FirstId  int
@@ -51,6 +33,7 @@ type CalendarPair struct {
 }
 
 // check everything brutally
+// Complexity O(n^2)
 func FindOverlapPairsBrutal(evts CalendarEvents) (ret []CalendarPair) {
 	length := len(evts)
 
@@ -70,8 +53,27 @@ func FindOverlapPairsBrutal(evts CalendarEvents) (ret []CalendarPair) {
 	return
 }
 
+// Complexity O(n^2)
+// using Golang's sort interface
+// we sort the calendar events by using Start date
+type CalendarEvents []CalendarEvent
+
+func (c CalendarEvents) Len() int {
+	return len(c)
+}
+
+func (c CalendarEvents) Swap(i, j int) {
+	temp := c[i]
+	c[i] = c[j]
+	c[j] = temp
+}
+
+func (c CalendarEvents) Less(i, j int) bool {
+	return c[i].Start < c[j].Start
+}
+
 // we sort the events for faster check
-func FindOverlapPairs(evts CalendarEvents) (ret []CalendarPair) {
+func FindOverlapPairsSort(evts CalendarEvents) (ret []CalendarPair) {
 	length := len(evts)
 
 	// if the input is less then 2 events then there must be no overlaps
@@ -101,4 +103,181 @@ func FindOverlapPairs(evts CalendarEvents) (ret []CalendarPair) {
 	}
 	return
 
+}
+
+// Segment struct
+
+type Segment struct {
+	Start int64
+	End   int64
+	Ids   []int
+}
+
+func (c *Segment) ToString() string {
+	return fmt.Sprintf("Segment %d -> %d with ids: %v", c.Start, c.End, c.Ids)
+}
+
+func (c *Segment) IsWithin(value int64) int {
+	if value < c.Start {
+		return -1
+	} else if value > c.End {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func (c *Segment) SplitStart(evt CalendarEvent) []Segment {
+	if c.Start == evt.Start {
+		return []Segment{{c.Start, c.End, append(c.Ids, evt.Id)}}
+	} else {
+		return []Segment{
+			{c.Start, evt.Start - 1, c.Ids},
+			{evt.Start, c.End, append(c.Ids, evt.Id)},
+		}
+	}
+
+}
+
+func (c *Segment) SplitWithin(evt CalendarEvent) []Segment {
+	if c.Start == evt.Start && c.End == evt.End {
+		return []Segment{
+			{c.Start, c.End, append(c.Ids, evt.Id)},
+		}
+	} else if c.Start == evt.Start {
+		return []Segment{
+			{c.Start, evt.End, append(c.Ids, evt.Id)},
+			{evt.End + 1, c.End, c.Ids},
+		}
+	} else if c.End == evt.End {
+		return []Segment{
+			{c.Start, evt.Start - 1, c.Ids},
+			{evt.Start, c.End, append(c.Ids, evt.Id)},
+		}
+	} else {
+		return []Segment{
+			{c.Start, evt.Start - 1, c.Ids},
+			{evt.Start, evt.End, append(c.Ids, evt.Id)},
+			{evt.End + 1, c.End, c.Ids},
+		}
+	}
+
+}
+
+func (c *Segment) SplitEnd(evt CalendarEvent) []Segment {
+	if c.End == evt.End {
+		return []Segment{
+			{c.Start, c.End, append(c.Ids, evt.Id)},
+		}
+	} else {
+		return []Segment{
+			{c.Start, evt.End, append(c.Ids, evt.Id)},
+			{evt.End + 1, c.End, c.Ids},
+		}
+	}
+
+}
+
+type Segments []Segment
+
+func (c Segments) len() int {
+	return len(c)
+}
+func (c Segments) FindSeg(value int64) (idx int) {
+	idx = len(c) / 2
+	for {
+		result := c[idx].IsWithin(value)
+		if result < 0 {
+			// search left
+			idx--
+			if idx < 0 {
+				// underflow
+				return
+			}
+			idx = idx / 2
+		} else if result > 0 {
+			// search right
+			idx++
+			if idx >= len(c) {
+				// overflow
+				return
+			}
+			idx = (idx + +1 + len(c)) / 2
+		} else {
+			//found it
+			return
+		}
+	}
+}
+
+func FindOverlapPairsSeg(evts CalendarEvents) (ret []CalendarPair) {
+	length := len(evts)
+
+	// if the input is less then 2 events then there must be no overlaps
+	if length < 2 {
+		return
+	}
+
+	segs := Segments{{Start: evts[0].Start, End: evts[0].End, Ids: []int{evts[0].Id}}}
+	for i := 1; i < length; i++ {
+		evt := evts[i]
+		// find the segment which include start
+		idx := segs.FindSeg(evt.Start)
+		if idx < 0 {
+			// underflow
+			if evt.End < segs[0].Start {
+				temp := Segments{Segment{Start: evt.Start, End: evt.End, Ids: []int{evt.Id}}}
+				segs = append(temp, segs...)
+			} else {
+				for _, id := range segs[0].Ids {
+					ret = append(ret, CalendarPair{id, evt.Id})
+				}
+				temp := Segments{Segment{Start: evt.Start, End: segs[0].Start - 1, Ids: []int{evt.Id}}}
+				temp = append(temp, segs[0].SplitEnd(evt)...)
+				segs = append(temp, segs[1:]...)
+			}
+		} else if idx == segs.len() {
+			// overflow
+			segs = append(segs, Segment{Start: evt.Start, End: evt.End, Ids: []int{evt.Id}})
+		} else {
+			// within
+			if evt.End <= segs[idx].End {
+				for _, id := range segs[idx].Ids {
+					ret = append(ret, CalendarPair{id, evt.Id})
+				}
+				// total within
+				temp := append(segs[:idx], segs[idx].SplitEnd(evt)...)
+				segs = append(temp, segs[idx+1:]...)
+			} else {
+				// we need search right for more overlaps
+				for _, id := range segs[idx].Ids {
+					ret = append(ret, CalendarPair{id, evt.Id})
+				}
+				temp := append(segs[:idx], segs[idx].SplitStart(evt)...)
+				end := false
+				for p := idx + 1; p < segs.len(); p++ {
+					for _, id := range segs[p].Ids {
+						ret = append(ret, CalendarPair{id, evt.Id})
+					}
+					if evt.End <= segs[p].End {
+						// last one
+						temp = append(temp, segs[p].SplitEnd(evt)...)
+						segs = append(temp, segs[p+1:]...)
+						end = true
+						break
+					} else {
+						segs[p].Ids = append(segs[p].Ids, evt.Id)
+						temp = append(temp, segs[p])
+					}
+				}
+				if !end {
+					// our new seg is over the last one
+					// so append a new seg here
+					segs = append(segs, Segment{Start: segs[segs.len()-1].End + 1, End: evt.End, Ids: []int{evt.Id}})
+				}
+			}
+		}
+
+	}
+	return
 }
--- calendar_test.go
+++ calendar_test.go
@@ -4,6 +4,34 @@ import (
 	"testing"
 )
 
+func compareTwoPairArrays(left, right []CalendarPair) bool {
+	if len(left) != len(right) {
+		return false
+	}
+	for _, evt := range left {
+		found := false
+		for i, temp := range right {
+			if evt.FirstId == temp.FirstId &&
+				evt.SecondId == temp.SecondId {
+				// found
+				found = true
+				if i+1 < len(right) {
+					if i > 0 {
+						right = append(right[:i-1], right[i+1:]...)
+					} else {
+						right = right[i+1:]
+					}
+				}
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
 func TestIsEventValid(t *testing.T) {
 	cal := CalendarEvent{0, 100, 200}
 	if !cal.IsValid() {
@@ -131,7 +159,7 @@ func TestCalendarBrutal(t *testing.T) {
 
 func TestCalendar(t *testing.T) {
 	events := []CalendarEvent{{0, 0, 100}, {1, 0, 100}}
-	ret := FindOverlapPairs(events)
+	ret := FindOverlapPairsSort(events)
 	if len(ret) != 1 {
 		t.Logf("it should return 1 pair instead of %d", len(ret))
 		t.FailNow()
@@ -142,7 +170,7 @@ func TestCalendar(t *testing.T) {
 	}
 
 	events = []CalendarEvent{{0, 0, 100}, {1, 99, 100}}
-	ret = FindOverlapPairs(events)
+	ret = FindOverlapPairsSort(events)
 	if len(ret) != 1 {
 		t.Logf("it should return 1 pair instead of %d", len(ret))
 		t.FailNow()
@@ -153,14 +181,14 @@ func TestCalendar(t *testing.T) {
 	}
 
 	events = []CalendarEvent{{0, 0, 100}, {1, 101, 200}}
-	ret = FindOverlapPairs(events)
+	ret = FindOverlapPairsSort(events)
 	if len(ret) != 0 {
 		t.Logf("it should has no overlap")
 		t.FailNow()
 	}
 
 	events = []CalendarEvent{{0, 0, 100}, {1, 101, 200}, {2, 0, 2000}}
-	ret = FindOverlapPairs(events)
+	ret = FindOverlapPairsSort(events)
 	if len(ret) != 2 {
 		t.Logf("it should return 2 pair instead of %d", len(ret))
 		t.FailNow()
@@ -172,7 +200,7 @@ func TestCalendar(t *testing.T) {
 	}
 
 	events = []CalendarEvent{{0, 0, 100}, {1, 101, 200}, {2, 0, 2000}, {3, 101, 200}}
-	ret = FindOverlapPairs(events)
+	ret = FindOverlapPairsSort(events)
 	if len(ret) != 4 {
 		t.Logf("it should return 4 pair instead of %d", len(ret))
 		t.FailNow()
@@ -187,6 +215,197 @@ func TestCalendar(t *testing.T) {
 		t.Logf("third pair %v", ret[2])
 		t.Logf("fourth pair %v", ret[3])
 
+		t.FailNow()
+	}
+}
+
+func TestSegment_IsWithin(t *testing.T) {
+	seg := Segment{100, 200, nil}
+	if seg.IsWithin(0) != -1 {
+		t.FailNow()
+	}
+	if seg.IsWithin(201) != 1 {
+		t.FailNow()
+	}
+	if seg.IsWithin(105) != 0 {
+		t.FailNow()
+	}
+}
+
+func TestSegment_SplitStart(t *testing.T) {
+	seg := Segment{100, 200, []int{0}}
+
+	ret := seg.SplitStart(CalendarEvent{1, 105, 203})
+	if len(ret) != 2 {
+		t.FailNow()
+	}
+	if ret[0].Start != seg.Start {
+		t.FailNow()
+	}
+	if ret[0].End != 105-1 {
+		t.FailNow()
+	}
+	if ret[0].Ids[0] != 0 {
+		t.FailNow()
+	}
+
+	if ret[1].Start != 105 {
+		t.FailNow()
+	}
+	if ret[1].End != seg.End {
+		t.FailNow()
+	}
+	if ret[1].Ids[0] != 0 || ret[1].Ids[1] != 1 {
+		t.FailNow()
+	}
+}
+
+func TestSegment_SplitEnd(t *testing.T) {
+	seg := Segment{100, 200, []int{0}}
+
+	ret := seg.SplitEnd(CalendarEvent{1, 6, 105})
+	if len(ret) != 2 {
+		t.FailNow()
+	}
+	if ret[0].Start != seg.Start {
+		t.FailNow()
+	}
+	if ret[0].End != 105 {
+		t.FailNow()
+	}
+	if ret[0].Ids[0] != 0 && ret[0].Ids[1] != 1 {
+		t.FailNow()
+	}
+
+	if ret[1].Start != 105+1 {
+		t.FailNow()
+	}
+	if ret[1].End != seg.End {
+		t.FailNow()
+	}
+	if ret[1].Ids[0] != 0 {
+		t.FailNow()
+	}
+}
+
+func TestSegment_SplitWithin(t *testing.T) {
+	seg := Segment{100, 200, []int{0}}
+
+	// total overlaps
+	ret := seg.SplitWithin(CalendarEvent{1, 100, 200})
+	if len(ret) != 1 {
+		t.FailNow()
+	}
+	if ret[0].Start != seg.Start ||
+		ret[0].End != seg.End ||
+		ret[0].Ids[0] != 0 ||
+		ret[0].Ids[1] != 1 {
+		t.Log("split within total overlaps failed")
+		t.FailNow()
+	}
+
+	ret = seg.SplitWithin(CalendarEvent{1, 100, 150})
+	if len(ret) != 2 {
+		t.FailNow()
+	}
+	if ret[0].Start != seg.Start ||
+		ret[0].End != 150 ||
+		ret[0].Ids[0] != 0 ||
+		ret[0].Ids[1] != 1 ||
+		ret[1].Start != 150+1 ||
+		ret[1].End != seg.End ||
+		ret[1].Ids[0] != 0 {
+		t.Log("split within same start overlaps failed")
+		t.FailNow()
+	}
+
+	ret = seg.SplitWithin(CalendarEvent{1, 150, 200})
+	if len(ret) != 2 {
+		t.FailNow()
+	}
+	if ret[0].Start != seg.Start ||
+		ret[0].End != 150-1 ||
+		ret[0].Ids[0] != 0 ||
+
+		ret[1].Start != 150 ||
+		ret[1].End != seg.End ||
+		ret[1].Ids[0] != 0 ||
+		ret[1].Ids[1] != 1 {
+		t.Log("split within same end overlaps failed")
+		t.FailNow()
+	}
+
+	ret = seg.SplitWithin(CalendarEvent{1, 150, 180})
+	if len(ret) != 3 {
+		t.FailNow()
+	}
+	if ret[0].Start != seg.Start ||
+		ret[0].End != 150-1 ||
+		ret[0].Ids[0] != 0 ||
+
+		ret[1].Start != 150 ||
+		ret[1].End != 180 ||
+		ret[1].Ids[0] != 0 ||
+		ret[1].Ids[1] != 1 ||
+
+		ret[2].Start != 180+1 ||
+		ret[2].End != seg.End ||
+		ret[2].Ids[0] != 0 {
+		t.Log("split within within overlaps failed")
+		t.FailNow()
+	}
+}
+
+func TestFindOverlapPairsSeg(t *testing.T) {
+	events := []CalendarEvent{{0, 0, 100}, {1, 0, 100}}
+	ret := FindOverlapPairsSeg(events)
+	if len(ret) != 1 {
+		t.Logf("it should return 1 pair instead of %d", len(ret))
+		t.FailNow()
+	}
+	if ret[0].FirstId != 0 || ret[0].SecondId != 1 {
+		t.Logf("firstId %d, secondId %d is wrong", ret[0].FirstId, ret[0].SecondId)
+		t.FailNow()
+	}
+
+	events = []CalendarEvent{{0, 0, 100}, {1, 99, 100}}
+	ret = FindOverlapPairsSeg(events)
+	if len(ret) != 1 {
+		t.Logf("it should return 1 pair instead of %d", len(ret))
+		t.FailNow()
+	}
+	if ret[0].FirstId != 0 || ret[0].SecondId != 1 {
+		t.Logf("firstId %d, secondId %d is wrong", ret[0].FirstId, ret[0].SecondId)
+		t.FailNow()
+	}
+
+	events = []CalendarEvent{{0, 0, 100}, {1, 101, 200}}
+	ret = FindOverlapPairsSeg(events)
+	if len(ret) != 0 {
+		t.Logf("it should has no overlap")
+		t.FailNow()
+	}
+
+	events = []CalendarEvent{{0, 0, 100}, {1, 101, 200}, {2, 0, 2000}}
+	ret = FindOverlapPairsSeg(events)
+	if len(ret) != 2 {
+		t.Logf("it should return 2 pair instead of %d", len(ret))
+		t.FailNow()
+	}
+	if ret[0].FirstId != 0 || ret[0].SecondId != 2 || ret[1].FirstId != 1 || ret[1].SecondId != 2 {
+		t.Logf("first pair %v", ret[0])
+		t.Logf("second pair %v", ret[1])
+		t.FailNow()
+	}
+
+	events = []CalendarEvent{{0, 0, 100}, {1, 101, 200}, {2, 0, 2000}, {3, 101, 200}}
+	ret = FindOverlapPairsSeg(events)
+	if len(ret) != 4 {
+		t.Logf("it should return 4 pair instead of %d", len(ret))
+		t.FailNow()
+	}
+	baseline := FindOverlapPairsBrutal(events)
+	if !compareTwoPairArrays(ret, baseline) {
 		t.FailNow()
 	}
 }
